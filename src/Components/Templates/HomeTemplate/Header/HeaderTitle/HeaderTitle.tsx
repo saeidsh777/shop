cd5b32466ac95069f65234732753913af06595aa
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2025-03-03 17:19:53 +0330

    FEAT: Create header title component

--- src/Components/Templates/HomeTemplate/Header/HeaderTitle/HeaderTitle.tsx
+++ src/Components/Templates/HomeTemplate/Header/HeaderTitle/HeaderTitle.tsx
@@ -3,11 +3,11 @@ import GlobalBtn from '@/Components/Modules/Btns/GlobalBtn/GlobalBtn';
 export default function HeaderTitle() {
     return (
         <div>
-            <h1 className="font-black text-5xl mb-8">
+            <h1 className="mb-8">
                 Exclusive Collections
                 <br /> For Z-Generation
             </h1>
-            <div className="flex items-center gap-2">
+            <div className="flex flex-col sm:flex-row sm:items-center gap-4">
                 <GlobalBtn type={1} href="/shop" title="Shop New"/>
                 <GlobalBtn type={2} href="/" title="Categories"/>
             </div>
