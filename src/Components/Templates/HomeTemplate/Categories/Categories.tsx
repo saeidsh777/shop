34eb70bff1bcc8fc30b06376027e469c2587e299
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2025-03-07 13:52:56 +0330

    FEAT: Categories

---
+++ src/Components/Templates/HomeTemplate/Categories/Categories.tsx
@@ -0,0 +1,10 @@
+import Slider from '@/Components/Modules/Slider/Slider';
+import React from 'react';
+
+export default function Categories() {
+    return (
+        <section className="container">
+            <Slider />
+        </section>
+    );
+}
