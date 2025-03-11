af35048c1624509e67523dbf57b03f2684958c9f
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2025-03-11 14:46:16 +0330

    FEAT: About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import AboutTemplate from '@/Components/Templates/AboutTemplate/AboutTemplate';
 
 export default function page() {
-  return (
-    <div>page</div>
-  )
+    return <AboutTemplate />;
 }
