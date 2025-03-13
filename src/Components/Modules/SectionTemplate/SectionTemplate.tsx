d3e0f6aee445b12bfbf3f0e713d50893f155c57f
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2025-03-13 15:02:35 +0330

    CHORE: Category page remove

--- src/Components/Modules/SectionTemplate/SectionTemplate.tsx
+++ src/Components/Modules/SectionTemplate/SectionTemplate.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 export default function SectionTemplate({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
-    return <div className='pt-20 py-12'>{children}</div>;
+    return <div className="pt-20 py-12">{children}</div>;
 }
